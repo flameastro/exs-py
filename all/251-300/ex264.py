eb3999dcf1ce7084b1430eea04b0758f3a4977c5
Flame <gabrielgoulartbnu@gmail.com>
2025-10-01 21:53:34 -0300

    Fiz mais um exercício

---
+++ all/251-300/ex264.py
@@ -0,0 +1,8 @@
+# ex264: Crie uma função que receba uma string como parâmetro e retorne a string fatiada pelos 150 primeiros caracteres e no fim um "Leia Mais" se houver mais que 150 caracteres.
+def leia_mais(string):
+    return f"{string[:150]} LEIA MAIS..." if len(string) >= 150 else string
+
+
+if __name__ == "__main__":
+    print(leia_mais("""Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos."""))  # Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus dui LEIA MAIS...
+    print(leia_mais("Python Test"))  # Python Test
