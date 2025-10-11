a280e58b33d2562d9e328836126c93e085d4b93a
flameastro <gabrielgoulartbnu@gmail.com>
2025-10-10 21:02:42 -0300

    ex272

---
+++ all/251-300/ex272.py
@@ -0,0 +1,6 @@
+# ex272: Escreva um programa que leia um número e imprima a si mesmo, o seu quadrado e o seu cubo.
+numero = int(input("Digite um número inteiro: "))
+
+print(f"O número digitado foi: {numero}")
+print(f"O quadrado do número digitado é: {numero ** 2}")
+print(f"O cubo do número digitado é: {numero ** 3}")
--- main.py
+++ main.py
@@ -4740,3 +4740,12 @@ elif idade > 64:
     classificacao = "Idoso"
 
 print(f"Sua classificação é: {classificacao}")
+
+
+# ex272: Escreva um programa que leia um número e imprima a si mesmo, o seu quadrado e o seu cubo.
+numero = int(input("Digite um número inteiro: "))
+
+print(f"O número digitado foi: {numero}")
+print(f"O quadrado do número digitado é: {numero ** 2}")
+print(f"O cubo do número digitado é: {numero ** 3}")
+
