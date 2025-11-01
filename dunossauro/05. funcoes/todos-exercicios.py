943949a8a9b50801931bf293364b773e9292d38d
flameastro <gabrielgoulartbnu@gmail.com>
2025-11-01 10:49:26 -0300

    ex005

--- dunossauro/05. funcoes/todos-exercicios.py
+++ dunossauro/05. funcoes/todos-exercicios.py
@@ -92,3 +92,14 @@ if __name__ == "__main__":
     print(retorna_caractere(13))  # P
     print(retorna_caractere(0))  # N
     print(retorna_caractere(-5))  # N
+
+
+# ex005: Faça um programa com uma função chamada soma_imposto. A função possui dois parâmetros formais: taxa_imposto, que é a quantia de imposto sobre vendas expressas em porcentagem, e custo, que é o custo de um item antes do imposto. A função "altera" o valor de custo para incluir o imposto sobre vendas.
+def soma_imposto(custo: float, taxa_imposto: float):
+    return custo - (custo * taxa_imposto)
+
+
+if __name__ == "__main__":
+    print(soma_imposto(1250.99, 0.4))  # 750.594
+    print(soma_imposto(25350, 0.15))  # 21547.5
+    print(soma_imposto(785.8, 0.45))  # 432.18999999999994
