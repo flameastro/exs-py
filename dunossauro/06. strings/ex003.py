8a6c7897c90e6d7b2c2cc1c059f59d796efc9787
flameastro <gabrielgoulartbnu@gmail.com>
2026-02-18 17:01:09 -0300

    Adiciona exercício 6.3

---
+++ dunossauro/06. strings/ex003.py
@@ -0,0 +1,10 @@
+# ex003: Nome na vertical. Faça um programa que solicite o nome do usuário e imprima-o na vertical.
+# F
+# U
+# L
+# A
+# N
+# O
+nome = input("NOME: ")
+for letra in nome:
+    print(letra)
