acbfe7489428904f88042880bb63789a74470f13
flameastro <gabrielgoulartbnu@gmail.com>
2026-02-18 17:55:18 -0300

    Adiciona exercício 6.5

---
+++ dunossauro/06. strings/ex005.py
@@ -0,0 +1,10 @@
+# ex005: Nome na vertical em escada invertida. Altere o programa anterior de modo que a escada seja invertida.
+# FULANO
+# FULAN
+# FULA
+# FUL
+# FU
+# F
+nome = input("NOME: ")
+for x in range(0, len(nome)):
+    print(nome[x:])
