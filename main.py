b5305dceb4db68306ba18e50c2763baeec7b3e14
flameastro <gabrielgoulartbnu@gmail.com>
2026-02-19 09:57:20 -0300

    Adiciona exercício 294

--- main.py
+++ main.py
@@ -5270,3 +5270,24 @@ if __name__ == "__main__":
     print(inverse([1, 2, 3]))
     print(inverse([10, 5, 0]))
     print(inverse([5, 6, 7, 1, 2]))
+
+
+# ex294: Create a function to print elements from a given list present at odd index positions
+# Given:
+# my_list = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+# Expected output: [20, 40, 60, 80, 100]
+
+def oddPositions(array):
+    odd_array = []
+
+    for x in range(len(array)):
+        if x % 2 != 0:
+            odd_array.append(array[x])
+
+    return odd_array
+
+
+if __name__ == "__main__":
+    print(oddPositions([1, 2, 3, 4, 5]))  # [2, 4]
+    print(oddPositions([10, 20, 30, 40, 50, 60, 70, 80, 90, 100]))  # [20, 40, 60, 80, 100]
+    print(oddPositions([25, 12, 34, 5]))  # [12, 5]
