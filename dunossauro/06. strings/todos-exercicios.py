c28ab2049f74e53a17bf47920326f00c12299a72
flameastro <gabrielgoulartbnu@gmail.com>
2026-02-20 11:36:00 -0300

    Adiciona exercício 6.8

--- dunossauro/06. strings/todos-exercicios.py
+++ dunossauro/06. strings/todos-exercicios.py
@@ -106,3 +106,12 @@ for vogal in VOGAIS:
 
 print(f"Aparecem {sum([string.count(x) for x in VOGAIS])} vogais na string.")
 print(f"A string possui {string.count(" ")} espaços em branco.")
+
+
+#ex008: Um palíndromo é uma seqüência de caracteres cuja leitura é idêntica se feita da direita para esquerda ou vice−versa. Por exemplo: OSSO e OVO são palíndromos. Em textos mais complexos os espaços e pontuação são ignorados. A frase SUBI NO ONIBUS é o exemplo de uma frase palíndroma onde os espaços foram ignorados. Faça um programa que leia uma seqüência de caracteres, mostre−a e diga se é um palíndromo ou não.
+palavra = input("Palavra: ").replace(" ", "")
+
+if palavra == palavra[::-1]:
+    print("A frase é um palíndromo.")
+else:
+    print("A frase não é um palíndromo.")
