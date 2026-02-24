95fb4af66c216e69472683cff519144ccead56c0
flameastro <gabrielgoulartbnu@gmail.com>
2026-02-24 12:51:44 -0300

    Adiciona exercício 8.3

--- dunossauro/08. classes/todos-exercicios.py
+++ dunossauro/08. classes/todos-exercicios.py
@@ -44,3 +44,35 @@ if __name__ == "__main__":
     print(q1.calcular_area())  # 156.25
     print(q1.alterar_valor_lado(21.4))  # Tamanho do lado alterado com sucesso.
     print(q1.calcular_area())  # 457.9599999999999
+
+
+# ex003: Crie uma classe que modele um retângulo:
+# Atributos: Lado_a, Lado_b (ou Comprimento e Largura, ou Base e Altura, a escolher)
+# Métodos: Mudar valor dos lados, Retornar valor dos lados, calcular Área e calcular Perímetro;
+class Retangulo:
+    def __init__(self, base: float, altura: float):
+        self.base = base
+        self.altura = altura
+
+    def alterar_valor(self, valor_base: float, valor_altura: float):
+        self.base = valor_base
+        self.altura = valor_altura
+
+    def retornar_valor(self):
+        return self.base, self.altura
+
+    def calcular_area(self):
+        return self.base * self.altura
+
+    def calcular_perimetro(self):
+        return (self.base + self.altura) * 2
+
+if __name__ == "__main__":
+    r1 = Retangulo(12.4, 25.4)
+    print(r1.alterar_valor(26.5, 12.3))
+    print(r1.calcular_area())  # 325.95000000000005
+    print(r1.calcular_perimetro())  # 77.6
+
+    r2 = Retangulo(23.4, 12.3)
+    print(r2.calcular_area())  # 287.82
+    print(r2.calcular_perimetro())  # 71.4
