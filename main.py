d357262fdfad51555596c9682ba4515ef71cc00e
flameastro <gabrielgoulartbnu@gmail.com>
2026-02-27 13:33:15 -0300

    Adiciona exercício 306

--- main.py
+++ main.py
@@ -5553,3 +5553,17 @@ while pais_a < pais_b:
 
 
 print(f"Quantidade de anos: {anos}")
+
+
+# ex306: Elaborar um programa que leia uma matriz A do tipo vetor com dez elementos inteiros positivos. Construir uma matriz B de mesmos tipo e dimensão, em que cada elemento da matriz B deve ser o valor negativo do elemento correspondente da matriz A. Desta forma, se em A[1] estiver armazenado o elemento 8, deve estar em B[1] o valor –8 e assim por diante. Apresentar os elementos da matriz B.
+matrizA = []
+matrizB = []
+
+for x in range(10):
+    numero = int(input("Digite um número inteiro e positivo: "))
+    matrizA.append(numero)
+
+for x in range(10):
+    matrizB.append(matrizA[x] * -1)
+
+print(matrizB)
