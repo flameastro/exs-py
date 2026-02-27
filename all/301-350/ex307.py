0b1dc234a8f6ae51eb69f7ceb6fba42f10758ff9
flameastro <gabrielgoulartbnu@gmail.com>
2026-02-27 13:39:42 -0300

    Corrije sintaxe do exercício 307

--- all/301-350/ex307.py
+++ all/301-350/ex307.py
@@ -1,5 +1,6 @@
 # ex307: Elaborar um programa que leia 15 elementos inteiros de uma matriz A do tipo vetor. Construir uma matriz B de mesmo tipo, observando a seguinte lei de formação: “todo elemento da matriz B deve ser o quadrado do elemento da matriz A correspondente”. Apresentar os elementos das matrizes A e B.
-matrizA = matrizB = []
+matrizA = []
+matrizB = []
 
 for x in range(15):
     numero = int(input("Digite um número inteiro: "))
