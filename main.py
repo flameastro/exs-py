7edc10dbcfe63fdd3d68bee663597945970ee2d8
flameastro <gabrielgoulartbnu@gmail.com>
2026-02-27 13:36:37 -0300

    Adiciona exercício 307

--- main.py
+++ main.py
@@ -5567,3 +5567,16 @@ for x in range(10):
     matrizB.append(matrizA[x] * -1)
 
 print(matrizB)
+
+
+# ex307: Elaborar um programa que leia 15 elementos inteiros de uma matriz A do tipo vetor. Construir uma matriz B de mesmo tipo, observando a seguinte lei de formação: “todo elemento da matriz B deve ser o quadrado do elemento da matriz A correspondente”. Apresentar os elementos das matrizes A e B.
+matrizA = matrizB = []
+
+for x in range(15):
+    numero = int(input("Digite um número inteiro: "))
+    matrizA.append(numero)
+
+for x in range(15):
+    matrizB.append(matrizA[x] ** 2)
+
+print(matrizB)
