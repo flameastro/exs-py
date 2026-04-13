de32c8abc472c4a0871c0809f5c049a2267a0901
flameastro <gabrielgoulartbnu@gmail.com>
2026-04-13 14:02:36 -0300

    Adiciona exercício 314

--- main.py
+++ main.py
@@ -5677,3 +5677,11 @@ porcentagem_reajuste = 8.75
 
 novo_salario = salario + (salario * (porcentagem_reajuste / 100))
 print(f"O novo salário do funcionário é R${novo_salario}")
+
+
+# ex314: # As lojas de um shopping center estão concedendo 10% de desconto no preço de qualquer produto. Faça um algoritmo que, a partir do valor fornecido, calcule e exiba o preço atual e o preço com o desconto.
+preco = float(input("Digite o valor do preço do produto: R$"))
+porcentagem_desconto = 10
+
+preco_com_desconto = preco - (preco * (porcentagem_desconto / 100))
+print(f"O novo preço com desconto é de R${preco_com_desconto}")
