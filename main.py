bf3bc5409590eb95083d5b3c981e3d2544cfd690
flameastro <gabrielgoulartbnu@gmail.com>
2026-04-13 14:01:57 -0300

    Adiciona exercício 313

--- main.py
+++ main.py
@@ -5669,3 +5669,11 @@ for l in matrizA:
 
 print(f"Quantidade de números pares: {pares}")
 print(f"Quantidade de números ímpares: {impares}")
+
+
+# ex313: Uma empresa concederá um reajuste salarial de 8,75% no próximo mês. Sua missão é elaborar um algoritmo que, a partir de dados inseridos pelo usuário, calcule o salário reajustado de um funcionário, exibindo, como resultado, seu nome, o valor de seu salário atual e o valor do salário reajustado.
+salario = float(input("Insira o salário R$: "))
+porcentagem_reajuste = 8.75
+
+novo_salario = salario + (salario * (porcentagem_reajuste / 100))
+print(f"O novo salário do funcionário é R${novo_salario}")
