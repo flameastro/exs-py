769eb3d09e4370548e7ab71737e5737c69deefe4
flameastro <gabrielgoulartbnu@gmail.com>
2026-06-11 21:23:56 -0300

    fix: erro de lógica (ex345)

--- all/301-350/ex345.py
+++ all/301-350/ex345.py
@@ -1,7 +1,7 @@
 # ex345: Crie uma função que receba um valor em horas como parâmetro no formato HH:MM:SS e retorne a soma dos segundos das horas, minutos e segundos
 def time_conversor(hour):
     hour = hour.split(":")
-    return (int(hour[0]) * 3600) + (int(hour[1]) * 60) + int(hour[0])
+    return (int(hour[0]) * 3600) + (int(hour[1]) * 60) + int(hour[2])
 
 if __name__ == "__main__":
     print(time_conversor("16:04:33"))
